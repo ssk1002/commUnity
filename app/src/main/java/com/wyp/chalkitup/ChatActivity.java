8b40027507d3b5295fa788de71ca319b48befe58
William Young <wy537@nyu.edu>
2017-02-18 15:54:06 -0500

    Chat Page Template

---
+++ app/src/main/java/com/wyp/chalkitup/ChatActivity.java
@@ -0,0 +1,14 @@
+package com.wyp.chalkitup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
